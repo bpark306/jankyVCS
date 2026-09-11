c6ebc9c19f2fbd0e648b9c52b17c24bbea904b17
brian <bpark4684@gmail.com>
2026-09-10 22:14:43 -0400

    Created cli subparser commands

--- jankyVCS.py
+++ jankyVCS.py
@@ -13,3 +13,20 @@ try:
     import grp, pwd
 except ModuleNotFoundError:
     pass
+
+argparser = argparse.ArgumentParser(description="The jankiest version control system. Embrace the jank!")
+
+argsubparsers = argparser.add_subpaersers(
+    title="Commands", 
+    dest="command", 
+    required=True
+);
+
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+    match args.command:
+    case "init" : cmd_init(args)
+    case "add" : cmd_add(args)
+    case "checkout" : cmd_checkout(args)
+    case "commit" : cmd_commit(args)
+    case _ : print("Unknown command. Type --help for list of valid commands")
