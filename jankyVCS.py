cada1fa9b0807fcde91cbf0ffb7ed77cb4feb186
brian <bpark4684@gmail.com>
2026-09-11 04:27:09 -0400

    Implemented ./janky-vcs init in the cli

--- jankyVCS.py
+++ jankyVCS.py
@@ -1,10 +1,10 @@
+from repository import GitRepository
 import argparse
-import tomllib
-from datetime import datetime
+import configparser
 import pathspec
+from datetime import datetime
 import hashlib
 from math import ceil
-import os
 import re
 import sys
 import zstandard as zstd
@@ -16,17 +16,29 @@ except ModuleNotFoundError:
 
 argparser = argparse.ArgumentParser(description="The jankiest version control system. Embrace the jank!")
 
-argsubparsers = argparser.add_subpaersers(
+argsubparsers = argparser.add_subparsers(
     title="Commands", 
     dest="command", 
     required=True
 );
 
+def cmd_init(args):
+    GitRepository.repo_create(args.path)
+
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
     match args.command:
-    case "init" : cmd_init(args)
-    case "add" : cmd_add(args)
-    case "checkout" : cmd_checkout(args)
-    case "commit" : cmd_commit(args)
-    case _ : print("Unknown command. Type --help for list of valid commands")
+        case "init" : cmd_init(args)
+        case "add" : cmd_add(args)
+        case "checkout" : cmd_checkout(args)
+        case "commit" : cmd_commit(args)
+        case _ : print("Unknown command. Type --help for list of valid commands")
+
+argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
+
+argsp.add_argument("path",
+                    metavar="directory",
+                    nargs="?",
+                    default=".",
+                    help="Where to create the repository.")
+
--- repository.py
+++ repository.py
@@ -12,11 +12,11 @@ class GitRepository (object) :
         self.gitdir = os.path.join(path, ".git")
 
         if not (force or os.path.isdir(self.gitdir)):
-            raise Exeception(f"Not a Git repository {path}")
+            raise Exception(f"Not a Git repository {path}")
 
         self.conf = configparser.ConfigParser()
 
-        cf = repo_file(self, "config")
+        cf = self.repo_file("config")
 
         if cf and os.path.exists(cf):
             self.conf.read([cf])
@@ -28,15 +28,15 @@ class GitRepository (object) :
             if vers !=0:
                 raise Exception(f"Unsupported repositoryformationversion: {vers}")
     
-    def repo_path(repo, *path):
-        return os.path.join(repo.gitdir, *path)
+    def repo_path(self, *path):
+        return os.path.join(self.gitdir, *path)
     
-    def repo_file(repo, *path, mkrdir=False):
-        if repo_dir(repo, *path[:-1], mkdir=mkdir):
-            return repo_path(repo, *path)
-            
-    def repo_dir(repo, *path, mkdir=False):
-        path = repo_path(repo, *path)
+    def repo_file(self, *path, mkdir=False):
+        if self.repo_dir(*path[:-1], mkdir=mkdir):
+            return self.repo_path(*path)
+
+    def repo_dir(self, *path, mkdir=False):
+        path = self.repo_path(*path)
 
         if os.path.exists(path):
             if (os.path.isdir(path)):
@@ -45,7 +45,48 @@ class GitRepository (object) :
                 raise Exception(f"Not a directory {path}")
 
         if mkdir:
-            os.markedirs(path)
+            os.makedirs(path)
             return path
         else:
             return None
+
+    @staticmethod
+    def repo_create(path):
+        repo = GitRepository(path, True)
+
+        if os.path.exists(repo.worktree):
+            if not os.path.isdir(repo.worktree):
+                raise Exception(f"{path} is not a directory!")
+            if os.path.exists(repo.gitdir) and os.listdir(repo.gitdir):
+                raise Exception(f"{path} already contains a Git repository!")
+        else:
+            os.makedirs(repo.worktree)
+
+        assert repo.repo_dir("branches", mkdir=True)
+        assert repo.repo_dir("objects", mkdir=True)
+        assert repo.repo_dir("refs", "tags", mkdir=True)
+        assert repo.repo_dir("refs", "heads", mkdir=True)
+
+        with open(repo.repo_file("description"), "w") as f:
+            f.write("Unnamed repository: edit this file 'description' to name the repository.\n")
+
+        with open(repo.repo_file("HEAD"), "w") as f:
+            f.write("ref: refs/heads/master\n")
+        
+        with open(repo.repo_file("config"), "w") as f:
+            config = GitRepository.repo_default_config()
+            config.write(f)
+
+        return repo
+
+    @staticmethod
+    def repo_default_config():
+        ret = configparser.ConfigParser()
+
+        ret.add_section("core")
+        ret.set("core", "repositoryformatversion", "0")
+        ret.set("core", "filemode", "false")
+        ret.set("core", "bare", "false")
+
+        return ret
+
